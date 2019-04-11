78dc6019bbdff8567471827277008dd51c38cc3a
k <kori@tulibu-dibu.com>
2019-04-11 15:18:57 +0300

    Uploaded main script
    Only warns for low battery level as of now. Need to add more states and configurable variables.

---
+++ disco.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while true; do  
+  battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+  status=$(cat /sys/class/power_supply/ADP1/online)
+  if [ $battery_level -le 20 ] && [ $status == "0" ]
+    then
+      notify-send -i /usr/share/icons/Numix/32/devices/battery-caution.svg "Battery low" "Battery level is ${battery_level}%!"
+  fi
+  sleep 300;
+done
