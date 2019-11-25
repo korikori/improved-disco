39b69eddb3d23acd8d332d13b106c62f04fa5b81
k <kori@tulibu-dibu.com>
2019-11-25 18:28:22 +0200

    Update disco.sh
    On the thinkpad, ADP1 is AC

--- disco.sh
+++ disco.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do  
   battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-  status=$(cat /sys/class/power_supply/ADP1/online)
+  status=$(cat /sys/class/power_supply/AC/online)
   if [ $battery_level -le 20 ] && [ $status == "0" ]
     then
       notify-send -i /usr/share/icons/Numix/32/devices/battery-caution.svg "Battery low" "Battery level is ${battery_level}%!"
