0129539a41f2afe41d0518a68f28bca72f816f9f
k <kori@tulibu-dibu.com>
2020-01-06 19:28:11 +0200

    New ThinkPad, new crap

--- disco.sh
+++ disco.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 while true; do  
-  battery_level=`acpi -b | grep -P -o -m 1 '[0-9]+(?=%)'`
+  battery1_level=`acpi -b | grep -P -o -m 1 '[0-9]+(?=%)'`
+  battery2_level=`acpi -b | tail -n 1 | grep -P -o -m 3 '[0-9]+(?=%)'`
+  if [ "$battery1_level" -gt "$battery2_level" ]; then
+    warning="Battery level is ${battery2_level}%!" 
+  else
+    warning="Battery level is ${battery1_level}%!" 
+  fi
   status=$(cat /sys/class/power_supply/AC/online)
-  if [ $battery_level -le 20 ] && [ $status == "0" ]
+  if [ $battery1_level -le 20 -o $battery2_level -le 20 ] && [ $status == "0" ]
     then
-      notify-send -i /usr/share/icons/Numix/32/devices/battery-caution.svg "Battery low" "Battery level is ${battery_level}%!"
+      notify-send -i /usr/share/icons/Numix/32/devices/battery-caution.svg "Low battery" "$warning"  
   fi
   sleep 300;
 done
